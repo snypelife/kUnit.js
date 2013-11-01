82aae309e8b869a2792ea6c7f97f14bdfc9c6f9f
Brett Rogerson <brettmrogerson@gmail.com>
2013-11-01 15:55:27 -0400

    Update kUnit.js
    removed unrelated utility function

--- kUnit.js
+++ kUnit.js
@@ -9,19 +9,7 @@
           'a': 97,'b': 98,'c': 99,'d': 100,'e': 101,'f': 102,'g': 103,'h': 104,'i': 105,'j': 106,'k': 107,'l': 108,'m': 109,'n': 110,'o': 111,'p': 112,'q': 113,'r': 114,'s': 115,'t': 116,'u': 117,'v': 118,'w': 119,'x': 120,'y': 121,'z': 122,'1': 49,'2': 50,'3': 51,'4': 52,'5': 53,'6': 54,'7': 55,'8': 56,'9': 57,'0': 58,'up': 38,'down': 40,'left': 37,'right': 39
       }
       , util = {
-            buildModal: function (name) {
-              if (document.querySelector('#' + name)) {
-                return
-              }
-              var modal = document.createElement('div')
-                , container = document.createElement('div')
-
-              modal.id = name
-              modal.className = 'modal hide fade'
-              container.className = 'modal-container'
-              modal.appendChild(container)
-              document.body.appendChild(modal)
-            }
+            exampleUtilityFunc: function (param) {}
         }
 
       , runTest = function (evt) {
